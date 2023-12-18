04fdf282879a5eba5411668af46f42fcb1365594
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2023-12-18 17:59:40 +0530

    Added delete method

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -69,5 +69,18 @@ namespace Catalog.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            repository.DeleteItem(id);
+            return NoContent();
+        }
     }
 }
