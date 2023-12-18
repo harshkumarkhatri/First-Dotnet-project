049ab08816343edfd4e68bb499eb2476d47cd6a6
Harsh Kumar Khatri <harshkumarkhatri@NMAC-62.local>
2023-12-18 14:11:22 +0530

    Added getall, get by id, post

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,51 @@
+using Catalog.Dtos;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            return repository.GetItems().Select(item => item.AsDto());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return item.AsDto();
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto){
+            Item item = new(){
+                Id= Guid.NewGuid(),
+                CreatedDate = DateTimeOffset.UtcNow,
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+            };
+
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new {id = item.Id}, item.AsDto());
+
+        }
+    }
+}
